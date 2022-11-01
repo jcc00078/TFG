d069ffc8ff9e2b49faf5fa7cb6b98651d24bcc3d
Juan Carmona Cortés <jcc00078@red.ujaen.es>
2022-11-01 19:32:17 +0100

    init

---
+++ src/test/java/jcc00078/TFG/TfgApplicationTests.java
@@ -0,0 +1,13 @@
+package jcc00078.TFG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
